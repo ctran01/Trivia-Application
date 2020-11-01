02743be95d634c2509560d0834d7409d54cf31f6
ctran01 <chris@DESKTOP-6RFRI4D.localdomain>
2020-11-01 17:07:21 -0600

    Created trivia app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,75 @@
 import React, { useState } from "react";
 import Main from "./components/Main";
 import GameContext from "./context/GameContext";
-
+import data from "./data/questions.json";
 const App = () => {
   const [score, setScore] = useState(0);
   const [gameState, setGameState] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [questionNumber, setQuestionNumber] = useState();
+  const [currentQuestion, setCurrentQuestion] = useState({});
+  const [results, setResults] = useState(false);
 
+  const getQuestions = () => {
+    let questionList = [];
+    let questionCount = 0;
+    while (questionCount < 10) {
+      let randIndex = Math.floor(Math.random() * data.length);
+      let randQuestion = data[randIndex];
+      if (!questionList.includes(randQuestion)) {
+        questionList.push(randQuestion);
+        questionCount++;
+      }
+    }
+    return questionList;
+  };
+
+  const onStart = () => {
+    setGameState(true);
+    setQuestions(getQuestions);
+    setQuestionNumber(1);
+    setCurrentQuestion(questions[0]);
+  };
+
+  const onRestart = () => {
+    setQuestions(getQuestions);
+    setQuestionNumber(1);
+    setScore(0);
+    setCurrentQuestion(questions[0]);
+    setGameState(true);
+    setResults(false);
+  };
+
+  const nextQuestion = () => {
+    if (questionNumber < questions.length) {
+      setQuestionNumber(questionNumber + 1);
+      setCurrentQuestion(questions[questionNumber - 1]);
+    } else if (questionNumber + 1 > questions.length) {
+      setGameState(false);
+    }
+  };
+  const prevQuestion = () => {
+    if (questionNumber > 0)
+      setCurrentQuestion(questions.indexOf(currentQuestion) - 1);
+  };
   const context = {
     score,
     setScore,
     gameState,
     setGameState,
+    questions,
+    setQuestions,
+    getQuestions,
+    currentQuestion,
+    setCurrentQuestion,
+    onStart,
+    onRestart,
+    nextQuestion,
+    prevQuestion,
+    questionNumber,
+    setQuestionNumber,
+    results,
+    setResults,
   };
 
   return (
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,44 @@
-import React, { useContext } from "react";
-import TopMenu from "./TopMenu";
-
+import React, { useContext, useEffect, useState } from "react";
+import LeftMenu from "./LeftMenu";
+import GameContext from "../context/GameContext";
+import Question from "./Question";
 const Main = () => {
+  const {
+    questions,
+    setQuestions,
+    onStart,
+    setCurrentQuestion,
+    currentQuestion,
+    gameState,
+    getQuestions,
+  } = useContext(GameContext);
+
+  // const questionList = data.map((question) => {
+  //   return <div>{question.question}</div>;
+  // });
+  useEffect(() => {
+    setQuestions(getQuestions);
+  }, []);
+
   return (
-    <>
-      <TopMenu />
-      <div>Main</div>
-    </>
+    <div>
+      <LeftMenu />
+      <main className="main-content-container">
+        <h1 className="header">Trivia Game! </h1>
+        {gameState === false ? (
+          <>
+            <div className="main-content">
+              Welcome to my Trivia Game! Click start to begin!
+            </div>
+            <div>
+              <button onClick={onStart}>Start!</button>
+            </div>
+          </>
+        ) : (
+          <Question />
+        )}
+      </main>
+    </div>
   );
 };
 
