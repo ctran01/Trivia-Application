556dc82a5291f1bc2926412c3736ad6c096e3b99
ctran01 <chris@DESKTOP-6RFRI4D.localdomain>
2020-11-04 15:54:24 -0600

    Testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,38 @@
-import { render, screen } from "@testing-library/react";
+import { mount, shallow, configure } from "enzyme";
+import React from "react";
 import ReactDOM from "react-dom";
-import App, { getQuestions } from "./App";
+import App from "./App";
+import Main from "./components/Main";
+import Adapter from "enzyme-adapter-react-16";
+configure({ adapter: new Adapter() });
+
 describe("App", () => {
   it("renders without crashing", () => {
-    const div = document.createElement("div");
-    ReactDOM.render(<App />, div);
+    shallow(<App />);
+  });
+});
+
+describe("Main", () => {
+  let wrapper;
+  beforeAll(() => {
+    wrapper = mount(<App />);
+  });
+
+  afterAll(() => {
+    wrapper.unmount();
+  });
+
+  it("renders Header", () => {
+    const trivia = <h1>Welcome to my Trivia Game! Click start to begin!</h1>;
+    expect(wrapper.contains(trivia)).toEqual(true);
+  });
+
+  it("state has 10 questions", () => {
+    const onStart = jest.fn();
+    wrapper = mount(<App questions={[]} />);
+
+    wrapper.find(".start-button").simulate("click");
+
+    expect(wrapper.state(questions)).toHaveLength(numQuestions);
   });
 });
